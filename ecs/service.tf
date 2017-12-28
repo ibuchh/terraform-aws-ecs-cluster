991aaa7527ff1ee29857de39a48f4c83cd0cb7c7
Irshad A Buchh <ibuchh@gmail.com>
2017-12-28 14:38:54 -0600

    Changed the attributes in ecs service

--- ecs/service.tf
+++ ecs/service.tf
@@ -1,9 +1,11 @@
 resource "aws_ecs_service" "mesh-ecs-service" {
-  name            = "${var.ecs-service-name}"
-  iam_role        = "${var.ecs-service-role-arn}"
-  cluster         = "${aws_ecs_cluster.mesh-ecs-cluster.id}"
-  task_definition = "${aws_ecs_task_definition.mesh-sample-definition.arn}"
-  desired_count   = 1
+  name                               = "${var.ecs-service-name}"
+  iam_role                           = "${var.ecs-service-role-arn}"
+  cluster                            = "${aws_ecs_cluster.mesh-ecs-cluster.id}"
+  task_definition                    = "${aws_ecs_task_definition.mesh-sample-definition.arn}"
+  desired_count                      = 2
+  deployment_maximum_percent         = 100
+  deployment_minimum_healthy_percent = 0
 
   load_balancer {
     target_group_arn = "${var.ecs-target-group-arn}"
